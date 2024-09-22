9b71ca184516c2521c5dd9f75688aad2b563021a
Yusuke Wada <yusuke@kamawada.com>
2024-09-22 14:28:13 +0900

    chore: add `tsup.config.ts` and `watch` command (#7)

---
+++ tsup.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/vite-plugin.ts', 'src/dev.ts', 'src/cloudflare-pages.ts'],
+  external: ['../server', 'virtual:remix/server-build', 'hono', '@remix-run/cloudflare', 'vite'],
+  format: 'esm',
+  dts: true,
+})
