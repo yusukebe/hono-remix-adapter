4f6c49844d5af19a37678b29e4eca5266e59dd01
Yusuke Wada <yusuke@kamawada.com>
2024-11-20 22:39:44 +0900

    test: enable testing with Wrangler for Pages and Workers

---
+++ examples/cloudflare-workers/e2e.test.ts
@@ -0,0 +1,27 @@
+import { expect, test } from '@playwright/test'
+
+test('Should return 200 response - /', async ({ page }) => {
+  const response = await page.goto('/')
+  expect(response?.status()).toBe(200)
+
+  const headers = response?.headers() ?? {}
+  expect(headers['x-powered-by']).toBe('Remix and Hono')
+
+  const contentH1 = await page.textContent('h1')
+  expect(contentH1).toBe('Remix and Hono')
+
+  const contentH2 = await page.textContent('h2')
+  expect(contentH2).toBe('Var is My Value')
+
+  const contentH3 = await page.textContent('h3')
+  expect(contentH3).toBe('cf,ctx,caches are available')
+
+  const contentH4 = await page.textContent('h4')
+  expect(contentH4).toBe('Extra is stuff')
+})
+
+test('Should return 200 response - /api', async ({ page }) => {
+  const response = await page.goto('/api')
+  expect(response?.status()).toBe(200)
+  expect(await response?.json()).toEqual({ message: 'Hello', var: 'My Value' })
+})
