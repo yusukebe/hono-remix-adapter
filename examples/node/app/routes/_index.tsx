f30985918e658973f4255ac7938cd2f249680d7a
barak codes <barakcodes@gmail.com>
2024-12-03 22:52:35 +0300

    fix: escape vite asset import suffixes
    Add the regix to exclude all vite asset import suffixes,
    might be better done in the @hono/vite-dev-server, but this
    fixes the issue for now

--- examples/node/app/routes/_index.tsx
+++ examples/node/app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { LoaderFunctionArgs } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
+import logoDark from '/logo-dark.png?inline'
 
 export const loader = (args: LoaderFunctionArgs) => {
   const extra = args.context.extra
@@ -14,6 +15,12 @@ export default function Index() {
       <h1>Remix and Hono</h1>
       <h2>URL is {url}</h2>
       <h3>Extra is {extra}</h3>
+      <div >
+        <img
+          src={logoDark}
+          alt="Remix"
+        />
+      </div>
     </div>
   )
 }
